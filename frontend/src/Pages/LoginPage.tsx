b9a65fba015914a1551622462b292d87de6b9247
Sivadas Rajan <sivadasrajan@gmail.com>
2023-01-21 20:18:44 +0530

    minor ui

--- frontend/src/Pages/LoginPage.tsx
+++ frontend/src/Pages/LoginPage.tsx
@@ -4,25 +4,27 @@ import GuestLayout from '../Templates/GuestLayout';
 
 const LoginPage: React.FC = () => {
   return <GuestLayout>
-    <div className="card w-full bg-neutral text-neutral-content">
+    <div className="card bg-neutral text-neutral-content">
       <div className="card-body items-center text-center">
         <h2 className="card-title">Login</h2>
         <div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
-              <span className="label-text">What is your name?</span>
-              <span className="label-text-alt">Alt label</span>
+              <span className="label-text">Username</span>
             </label>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-            <label className="label">
-              <span className="label-text-alt">Alt label</span>
-              <span className="label-text-alt">Alt label</span>
-            </label>
+            <input type="text" placeholder="Enter Username" className="input input-bordered w-full max-w-xs" />
           </div>
         </div>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Accept</button>
-          <button className="btn btn-ghost">Deny</button>
+        <div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="Enter Password" className="input input-bordered w-full max-w-xs" />
+          </div>
+        </div>
+        <div className="card-actions justify-between w-full">
+          <button className="btn w-full btn-primary">Login</button>
         </div>
       </div>
     </div>
--- frontend/src/Templates/GuestLayout.tsx
+++ frontend/src/Templates/GuestLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'; // importing FunctionComponent
 
 
 const GuestLayout = ({children}: {children: React.ReactNode}) => {
-  return <div className='w-full'>
+  return <div className='flex justify-center w-full'>
       {children}
     </div>;
 }
