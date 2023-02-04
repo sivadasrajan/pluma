dd3110023ab02639336fa99aa8035a1575e0e7b6
Sivadas Rajan <sivadasrajan@gmail.com>
2023-02-04 16:14:56 +0530

    Routing iimprovements

---
+++ app/Controllers/LedgerController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controllers;
+
+
+
+use SivadasRajan\Pluma\JWT;
+use SivadasRajan\Pluma\Http\Request;
+use SivadasRajan\Pluma\Http\Response;
+use SivadasRajan\Pluma\Route\Controller;
+
+
+class LedgerController extends Controller{
+
+    protected $jwt;
+
+    public function __construct() {
+        
+    }
+    public function all(Request $request)
+    {
+        
+    }
+    public function create(Request $request)
+    {
+        
+    }
+    public function update(Request $request)
+    {
+        
+    }
+    public function delete(Request $request)
+    {
+        return new Response("EEEEEEE",200);
+    }
+    public function show(Request $request)
+    {
+        
+    }
+}
